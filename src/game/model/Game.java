d40f8e75438f21ee00ac9dfbb0b86d948dc71675
JeffBaucom <jeffbaucom@gmail.com>
2015-09-22 18:01:31 -0400

    added money handling in Turn.buyTile() for paid selection

--- src/game/model/Game.java
+++ src/game/model/Game.java
@@ -43,9 +43,9 @@ public class Game {
     }
 
     public int getPhase() {
-        if (roundCounter == 0) {
+        if (roundCounter <= 1) {
             return 0;
-        } else if ((roundCounter > 0) && (passCounter < players.length)) {
+        } else if ((roundCounter > 1) && (passCounter < players.length)) {
             return 1;
         } else {
             return 2;
