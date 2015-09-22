ed48f71f857dcc117a5cef0b475c21e3e66db8a9
JeffBaucom <jeffbaucom@gmail.com>
2015-09-22 17:56:48 -0400

    Added resources to Player

--- src/game/model/Player.java
+++ src/game/model/Player.java
@@ -6,13 +6,28 @@ public class Player {
     String name;
     Color color;
     Race race;
+    int money;
+    int food;
+    int energy;
+    int ore;
 
     public Player() {
     }
+
     public Player(String name, Color color, Race race) {
         this.name = name;
         this.color = color;
         this.race = race;
+        if (race == Race.FLAPPER) {
+            this.money = 1600;
+        } else if (race == Race.HUMAN) {
+            this.money = 600;
+        } else {
+            this.money = 1000;
+        }
+        this.food = 8;
+        this.energy = 4;
+        this.ore = 0;
     }
 
     public void setColor(Color color) { this.color = color; }
@@ -23,4 +38,37 @@ public class Player {
 
     public void setName(String name) { this.name = name; }
     public String getName() {return this.name; }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public int getOre() {
+        return ore;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public void setOre(int ore) {
+        this.ore = ore;
+    }
+
 }
