dc358f116f892208035410962699afca92987255
JeffBaucom <jeffbaucom@gmail.com>
2015-11-11 11:25:36 -0500

    add SellStoreTest

---
+++ src/game/test/SellStoreTest.java
@@ -0,0 +1,65 @@
+package game.test;
+
+import game.model.Game;
+import game.model.Player;
+import game.model.Store;
+import junit.framework.*;
+
+/**
+ * Created by ABUNAI on 11/11/15.
+ */
+public class SellStoreTest extends TestCase{
+    protected Game game;
+    protected Store store;
+
+
+
+    public void setUp() {
+        game = new Game(2, 0, "standard");
+        game.newPlayer(0, 0, "P1", "red", "Human");
+        game.newPlayer(1, 0, "P2", "yellow", "Human");
+        game.startGame();
+        store = game.getStore();
+    }
+
+    public void testSellFood() {
+        Player p = game.getCurrentPlayer();
+        int currFood = p.get("food");
+        int currMoney = p.get("money");
+        int currStoreFood = store.getStock("food");
+        int amount = 3;
+        boolean test = game.getTurn().sellStore("food", amount);
+        assertTrue(test);
+        assertTrue(store.getStock("food") == currStoreFood + amount);
+        assertTrue(p.get("money") == currMoney + (amount*store.getCost("food")));
+        assertTrue(p.get("food") == currFood - amount);
+    }
+
+    public void testSellAllFood() {
+        Player p = game.getCurrentPlayer();
+        int currFood = p.get("food");
+        int currMoney = p.get("money");
+        int currStoreFood = store.getStock("food");
+        int amount = currFood;
+        boolean test = game.getTurn().sellStore("food", amount);
+        assertTrue(test);
+        assertTrue(store.getStock("food") == currStoreFood + amount);
+        assertTrue(p.get("money") == currMoney + (amount*store.getCost("food")));
+        assertTrue(p.get("food") == currFood - amount);
+    }
+
+    public void testSellInsuffFood() {
+        Player p = game.getCurrentPlayer();
+        int currFood = p.get("food");
+        int currMoney = p.get("money");
+        int currStoreFood = store.getStock("food");
+        int amount = currFood + 1;
+        boolean test = game.getTurn().sellStore("food", amount);
+        assertFalse(test);
+        assertTrue(store.getStock("food") == currStoreFood);
+        assertTrue(p.get("money") == currMoney);
+        assertTrue(p.get("food") == currFood);
+
+    }
+
+}
