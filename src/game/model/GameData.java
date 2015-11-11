00e986bce04e34fd72398561d4ebe88d8956c612
JeffBaucom <jeffbaucom@gmail.com>
2015-11-11 15:51:58 -0500

    add javadocs and fix checkstyle errors for GameData

--- src/game/model/GameData.java
+++ src/game/model/GameData.java
@@ -1,21 +1,52 @@
 package game.model;
 
+/**
+ * Class that aggregates game information for saving.
+ */
 public class GameData {
-    int difficulty;
-    String mapType;
+    /**
+     * difficulty of the game as an int.
+     */
+    private int difficulty;
+    /**
+     * String of mapType i.e. "standard".
+     */
+    private String mapType;
 
-    int playerCounter, roundCounter, passCounter, phase, timeLeft;
-    boolean turnover;
+    /**
+     * integer representation of game data variables.
+     */
+    private int playerCounter, roundCounter, passCounter, phase, timeLeft;
+    /**
+     * boolean referring to the current turn status.
+     */
+    private boolean turnover;
 
-    Player[] players;
-    Tile[][] tiles;
+    /**
+     * player object array.
+     */
+    private  Player[] players;
+    /**
+     * tile object array.
+     */
+    private Tile[][] tiles;
 
-    String gameLog;
+    /**
+     * string of the game console log.
+     */
+    private String gameLog;
 
+    /**
+     *
+     */
     public GameData() {
     }
 
-    public GameData(Game game) {
+    /**
+     *
+     * @param game the game object
+     */
+    public GameData(final Game game) {
         this.difficulty = game.getDifficulty();
         this.mapType = game.getMapType();
 
@@ -32,51 +63,91 @@ public class GameData {
         this.gameLog = game.getGameLog();
     }
 
-    public String toString() {
-        return players.toString();
-    }
-
-    public int getDifficulty() {
+    /**
+     *
+     * @return integer representation of the game difficulty
+     */
+    public final int getDifficulty() {
         return difficulty;
     }
 
-    public String getMapType() {
+    /**
+     *
+     * @return string representation of the game map type
+     */
+    public final String getMapType() {
         return mapType;
     }
 
-    public int getPlayerCounter() {
+    /**
+     *
+     * @return integer representing the current player
+     */
+    public final int getPlayerCounter() {
         return playerCounter;
     }
 
-    public int getRoundCounter() {
+    /**
+     *
+     * @return integer representation of the current round
+     */
+    public final int getRoundCounter() {
         return roundCounter;
     }
 
-    public int getPassCounter() {
+    /**
+     *
+     * @return integer representation of the number of current number of passes
+     */
+    public final int getPassCounter() {
         return passCounter;
     }
 
-    public int getPhase() {
+    /**
+     *
+     * @return integer representing the current game phase
+     */
+    public final int getPhase() {
         return phase;
     }
 
-    public boolean isTurnover() {
+    /**
+     *
+     * @return boolean representing if the current turn is already over
+     */
+    public final boolean isTurnover() {
         return turnover;
     }
 
-    public int getTimeLeft() {
+    /**
+     *
+     * @return int representing the time left in the turn timer
+     */
+    public final int getTimeLeft() {
         return timeLeft;
     }
 
-    public Player[] getPlayers() {
+    /**
+     *
+     * @return array of player objects
+     */
+    public final Player[] getPlayers() {
         return players;
     }
 
-    public Tile[][] getTiles() {
+    /**
+     *
+     * @return 2D array of the tile objects in map
+     */
+    public final Tile[][] getTiles() {
         return tiles;
     }
 
-    public String getGameLog() {
+    /**
+     *
+     * @return string of current game log info
+     */
+    public final String getGameLog() {
         return gameLog;
     }
 }
